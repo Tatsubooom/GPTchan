7d02547cf1876374f73fd39374a5644d4bf334e6
Tatsuboom <2005tatsuya0920@gmail.com>
2026-04-14 04:15:55 +0900

    Add BackLogs

--- gpt_api.py
+++ gpt_api.py
@@ -8,13 +8,16 @@ def createTextResponse(input_message,personal_log):
     api_key = os.getenv("OPENAI_API_KEY")
     GPTclient = OpenAI()
 
-    rollfile = open('SubRolls.txt', 'r', encoding='utf-8')
-    rolltext = rollfile.read()
+    with open('SubRolls.txt', 'r', encoding='utf-8') as rollfile:
+        rolltext = rollfile.read()
+    
+    with open('BaclContext', 'r', encoding='utf-8') as contextfile:
+        backcontext = contextfile.read()
 
     try:
         response = GPTclient.responses.create(
         model="gpt-4.1",
-        instructions= f"日本語,口語調,100文字以内,{rolltext}\n{personal_log}",
+        instructions= f"日本語,口語調,100文字以内,{rolltext},あなたが所属するサークルの情報『{backcontext}』\n{personal_log}",
         input=input_message,
         max_output_tokens=400,
         temperature=0.2,
